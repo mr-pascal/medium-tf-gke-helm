96a0e8e51fd210010580788b0575b8eb6a662919
Pascal Zwikirsch <pascalrene.zwikirsch@gmail.com>
2022-11-26 15:00:02 +0100

    remove dead code

--- cluster.tf
+++ cluster.tf
@@ -2,13 +2,7 @@
 # Retrieve an access token as the Terraform runner
 data "google_client_config" "default" {}
 
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke-cluster.ca_certificate)
-}
 
-  
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "my-gke"
